c172b0848bec7bb4de3c5c5205292be5b7e8c71f
Sam MG Harish <147686318+sam-mg@users.noreply.github.com>
2025-01-13 17:50:52 +0530

    Update lu77U_SHA256/Custom_Print.py

--- lu77U_SHA256/Custom_Print.py
+++ lu77U_SHA256/Custom_Print.py
@@ -5,8 +5,8 @@ class CustomPrint:
     def __init__(self, text):
         self.text = text
 
-    def custom_print(text):
-        for char in text:
+    def custom_print(self):
+        for char in self.text:
             sys.stdout.write(char)
             sys.stdout.flush()
             time.sleep(0.1)
